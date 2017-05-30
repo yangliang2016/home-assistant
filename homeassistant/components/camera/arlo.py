496134c149d54881d0a34c04cadcd7e5af7f7bef
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-05-30 12:22:58 -0400

    Testing latest commit

--- homeassistant/components/camera/arlo.py
+++ homeassistant/components/camera/arlo.py
@@ -6,7 +6,6 @@ https://home-assistant.io/components/camera.arlo/
 """
 import asyncio
 import logging
-from datetime import timedelta
 import voluptuous as vol
 
 from homeassistant.helpers import config_validation as cv
@@ -14,6 +13,7 @@ from homeassistant.components.arlo import (
     DEFAULT_BRAND, DEFAULT_ENTITY_NAMESPACE)
 
 from homeassistant.components.camera import (Camera, PLATFORM_SCHEMA)
+from homeassistant.components.ffmpeg import DATA_FFMPEG
 from homeassistant.const import CONF_ENTITY_NAMESPACE
 from homeassistant.helpers.aiohttp_client import (
     async_aiohttp_proxy_stream)
@@ -23,34 +23,27 @@ DEPENDENCIES = ['arlo', 'ffmpeg']
 _LOGGER = logging.getLogger(__name__)
 
 CONF_FFMPEG_ARGUMENTS = 'ffmpeg_arguments'
-CONF_FFMPEG_BINARY = 'ffmpeg_binary'
-
-CONTENT_TYPE_HEADER = 'Content-Type'
-TIMEOUT = 5
-
-DEFAULT_FFMPEG_BINARY = '/usr/bin/ffmpeg'
-
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=90)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_ENTITY_NAMESPACE, default=DEFAULT_ENTITY_NAMESPACE):
         cv.string,
     vol.Optional(CONF_FFMPEG_ARGUMENTS, default=''):
         cv.string,
-    vol.Optional(CONF_FFMPEG_BINARY, default=DEFAULT_FFMPEG_BINARY):
-        cv.string,
 })
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+@asyncio.coroutine
+def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     """Set up an Arlo IP Camera."""
     arlo = hass.data.get('arlo')
+    if not arlo:
+        return False
 
     cameras = []
     for camera in arlo.cameras:
         cameras.append(ArloCam(hass, camera, config))
 
-    add_devices(cameras, True)
+    async_add_devices(cameras, True)
     return True
 
 
@@ -59,12 +52,12 @@ class ArloCam(Camera):
 
     def __init__(self, hass, camera, device_info):
         """Initialize an Arlo camera."""
-        super(ArloCam, self).__init__()
+        super().__init__()
+
         self._camera = camera
-        self._hass = hass
         self._name = self._camera.name
+        self._ffmpeg = hass.data[DATA_FFMPEG]
         self._ffmpeg_arguments = device_info.get(CONF_FFMPEG_ARGUMENTS)
-        self._ffmpeg_binary = device_info.get(CONF_FFMPEG_BINARY)
 
     def camera_image(self):
         """Return a still image reponse from the camera."""
@@ -78,7 +71,7 @@ class ArloCam(Camera):
         if not video:
             return
 
-        stream =  CameraMjpeg(self._ffmpeg_binary, loop=self.hass.loop)
+        stream = CameraMjpeg(self._ffmpeg.binary, loop=self.hass.loop)
         yield from stream.open_camera(
             video.video_url, extra_cmd=self._ffmpeg_arguments)
 
